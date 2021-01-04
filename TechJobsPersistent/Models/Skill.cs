6dd064b40e7fb37532ac83aa140af0dff60ff861
Mehmet Gokcek <gokcek50@gmail.com>
2021-01-03 20:52:35 -0600

    finished many to many ORM

--- TechJobsPersistent/Models/Skill.cs
+++ TechJobsPersistent/Models/Skill.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace TechJobsPersistent.Models
 {
     public class Skill
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
 
         public Skill()
-        {
-        }
+        {}
 
         public Skill(string name, string description)
         {
